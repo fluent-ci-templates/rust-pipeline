c4ad622c5f45699309dae6d8a18c4f696e36fb58
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-09 21:54:44 +0000

    export Dagger pipeline and jobs

--- example/dagger-ci.ts
+++ example/dagger-ci.ts
@@ -1,5 +1,5 @@
 import Client, { connect } from "@dagger.io/dagger";
-import { Dagger } from "https://deno.land/x/rust_pipeline/mod.ts";
+import { Dagger } from "https://deno.land/x/rust_pipeline@v0.1.1/mod.ts";
 
 const { build, test } = Dagger;
 
