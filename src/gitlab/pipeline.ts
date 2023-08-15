594e1fb91cce8b19f97b73c21718e1402143fee2
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-15 12:47:01 +0000

    feat: add github actions workflow generator

--- src/gitlab/pipeline.ts
+++
@@ -1,9 +0,0 @@
-import { GitlabCI } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
-import { build, test } from "./jobs.ts";
-
-const gitlabci = new GitlabCI()
-  .image("rust:latest")
-  .addJob("test", test)
-  .addJob("build", build);
-
-export default gitlabci;
