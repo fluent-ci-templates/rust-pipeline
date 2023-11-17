017fa55fb3e1e0faa79010e9fe740d9354529690
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-17 05:56:29 +0000

    use Dagger v0.9.3

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   build,
   test,
-} from "https://pkg.fluentci.io/rust_pipeline@v0.7.0/mod.ts";
+} from "https://pkg.fluentci.io/rust_pipeline@v0.8.0/mod.ts";
 
 await test();
 await build();
