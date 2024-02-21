b1720832f17542a8b14f59c9393617578b17a0fa
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-21 19:06:43 +0000

    make it compatible with Dagger v0.9.11

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   build,
   test,
-} from "https://pkg.fluentci.io/rust_pipeline@v0.8.3/mod.ts";
+} from "https://pkg.fluentci.io/rust_pipeline@v0.8.4/mod.ts";
 
 await test();
 await build();
