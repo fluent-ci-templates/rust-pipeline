5b275ce27564816bc5a5272c643b56966e3eaf37
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-08 05:34:24 +0000

    [plugin] use fluentci-pdk v0.1.9

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,13 +1,19 @@
 use extism_pdk::*;
 use fluentci_pdk::dag;
 
+use crate::helpers::set_envs;
+
+pub mod helpers;
+
 #[plugin_fn]
 pub fn clippy() -> FnResult<String> {
+    set_envs()?;
+
     let stdout = dag()
         .pipeline("clippy")?
         .pkgx()?
-        .with_exec(vec!["pkgx", "install", "curl", "wget"])?
-        .with_exec(vec!["type rustc > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"])?
+        .with_exec(vec!["curl", "wget"])?
+        .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"])?
         .with_exec(vec!["rustup", "component", "add", "clippy"])?
         .with_exec(vec!["cargo", "install", "clippy-sarif", "--version", "0.3.0"])?
         .with_exec(vec!["cargo", "install", "sarif-fmt", "--version", "0.3.0"])?
@@ -23,11 +29,13 @@ pub fn clippy() -> FnResult<String> {
 
 #[plugin_fn]
 pub fn llvmcov() -> FnResult<String> {
+    set_envs()?;
+
     let stdout = dag()
         .pipeline("llvmcov")?
         .pkgx()?
-        .with_exec(vec!["pkgx", "install", "curl", "wget"])?
-        .with_exec(vec!["type rustc > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"])?
+        .with_exec(vec!["curl", "wget"])?
+        .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"])?
         .with_exec(vec!["rustup", "component", "add", "llvm-tools"])?
         .with_exec(vec![
             "wget",
@@ -50,11 +58,13 @@ pub fn llvmcov() -> FnResult<String> {
 
 #[plugin_fn]
 pub fn test(args: String) -> FnResult<String> {
+    set_envs()?;
+
     let stdout = dag()
         .pipeline("test")?
         .pkgx()?
-        .with_exec(vec!["pkgx", "install", "curl", "wget"])?
-        .with_exec(vec!["type rustc > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"])?
+        .with_packages(vec!["curl", "wget"])?
+        .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"])?
         .with_exec(vec!["cargo", "test", &args])?
         .stdout()?;
     Ok(stdout)
@@ -62,11 +72,13 @@ pub fn test(args: String) -> FnResult<String> {
 
 #[plugin_fn]
 pub fn build(args: String) -> FnResult<String> {
+    set_envs()?;
+
     let stdout = dag()
         .pipeline("build")?
         .pkgx()?
         .with_exec(vec!["pkgx", "install", "curl"])?
-        .with_exec(vec!["type rustc > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"])?
+        .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"])?
         .with_exec(vec!["cargo", "build", &args])?
         .stdout()?;
     Ok(stdout)
