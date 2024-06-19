a8e8e1b7ecc8c4dc1b03abb92712d6c902004f41
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-06-19 13:46:42 +0000

    install cc if not installed

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -27,6 +27,7 @@ pub fn clippy() -> FnResult<String> {
         .pipeline("clippy")?
         .pkgx()?
         .with_packages(vec!["curl", "wget"])?
+        .with_exec(vec!["type cc > /dev/null || pkgx install gnu.org/gcc"])?
         .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"])?
         .with_exec(vec!["rustup", "component", "add", "clippy"])?
         .with_exec(vec!["PATH=$HOME/.cargo/bin:$PATH", "cargo", "install", "clippy-sarif", "--version", "0.3.0"])?
@@ -51,6 +52,7 @@ pub fn llvmcov() -> FnResult<String> {
         .pkgx()?
         .with_packages(vec!["curl", "wget"])?
         .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"])?
+        .with_exec(vec!["type cc > /dev/null || pkgx install gnu.org/gcc"])?
         .with_exec(vec!["rustup", "component", "add", "llvm-tools"])?
         .with_exec(vec![
             "wget",
@@ -80,6 +82,7 @@ pub fn test(args: String) -> FnResult<String> {
         .pipeline("test")?
         .pkgx()?
         .with_packages(vec!["curl"])?
+        .with_exec(vec!["type cc > /dev/null || pkgx install gnu.org/gcc"])?
         .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"])?
         .with_exec(vec!["PATH=$HOME/.cargo/bin:$PATH", "cargo", "test", &args])?
         .stdout()?;
@@ -94,6 +97,7 @@ pub fn build(args: String) -> FnResult<String> {
         .pipeline("build")?
         .pkgx()?
         .with_packages(vec!["curl"])?
+        .with_exec(vec!["type cc > /dev/null || pkgx install gnu.org/gcc"])?
         .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"])?
         .with_exec(vec!["PATH=$HOME/.cargo/bin:$PATH", "cargo", "build", &args])?
         .stdout()?;
@@ -108,6 +112,7 @@ pub fn target_add(args: String) -> FnResult<String> {
         .pipeline("target_add")?
         .pkgx()?
         .with_packages(vec!["curl"])?
+        .with_exec(vec!["type cc > /dev/null || pkgx install gnu.org/gcc"])?
         .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"])?
         .with_exec(vec!["PATH=$HOME/.cargo/bin:$PATH", "rustup", "target", "add", &args])?
         .stdout()?;
@@ -122,6 +127,7 @@ pub fn component_add(args: String) -> FnResult<String> {
         .pipeline("component_add")?
         .pkgx()?
         .with_packages(vec![ "curl"])?
+        .with_exec(vec!["type cc > /dev/null || pkgx install gnu.org/gcc"])?
         .with_exec(vec!["type rustup > /dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"])?
         .with_exec(vec!["PATH=$HOME/.cargo/bin:$PATH", "rustup", "component", "add", &args])?
         .stdout()?;
